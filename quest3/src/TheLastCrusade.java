4293b6cf44d5312ea52a9aaeee8944ad9c172f10
Aditpa <124455792+Aditpa@users.noreply.github.com>
2023-08-17 15:09:54 +0200

    quest3

---
+++ quest3/src/TheLastCrusade.java
@@ -0,0 +1,20 @@
+public class TheLastCrusade {
+
+
+    public static void main(String[] args) {
+    String name="Henry Jones";
+    boolean watched= false;
+    int releaseDate = 1989;
+    float rating= 8.2f;
+
+
+        System.out.println("name = " + name);
+        System.out.println("watched = " + watched);
+        System.out.println("releaseDate = " + releaseDate);
+        System.out.println("rating = " + rating);
+
+    }
+
+
+
+}
